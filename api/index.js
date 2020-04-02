9fde60009626692556b3c26aec27ee40e1b530bb
himanshu9345 <hphimanshu6@gmail.com>
2020-04-02 19:58:26 -0400

    node as server

---
+++ api/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/",(req,res)=>{
+    res.render('index',{
+        name:"Himanshu"
+    });
+});
+
+export default router;

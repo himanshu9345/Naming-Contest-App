d4d8c039be145376feb8b708dfadc0fb56dd34bf
himanshu9345 <hphimanshu6@gmail.com>
2020-04-26 04:04:23 -0400

    React props and components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+const App =(props)=>{
+    return(
+        <h2 className="text-center">
+            {props.msg}
+        </h2>
+    );
+};
+
+App.propTypes={
+    msg: PropTypes.string
+};
+
+App.defaultProps={
+    msg:"Himanshu!!!"
+};
 
 ReactDOM.render(
-    React.createElement('h2',null,'Hello React'),
+    <App msg="gg"/>,
     document.getElementById('root')
-)
+);

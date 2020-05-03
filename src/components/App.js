e3673da75fc2deaae32da0a2b25b9bb2bb7a4c8a
himanshu9345 <hphimanshu6@gmail.com>
2020-05-03 02:36:50 -0400

    added component state

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,27 @@ import Header from "./Header";
 import React from 'react';
 
 
-const App =()=>{
-    return(
-        <div className="App">
-        <Header msg="Naming Contests"/>
-        <div>
-            ....
-        </div>
-        </div>
-    );
-};
+
+class App extends React.Component{
+    // constructor(props){
+    //     super(props);
+    //     this.state={test:45}
+    // }
+    state = {
+        pageHeader: 'Naming Contests'
+    }
+    render(){
+
+        return(
+            <div className="App">
+            <Header msg={this.state.pageHeader}/>
+            <div>
+                ....
+            </div>
+            </div>
+        );
+    }
+}
+
 
 export default App;
